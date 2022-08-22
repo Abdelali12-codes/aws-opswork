3446deff22a42b9096808a8d74ec37263685b41a
Abdelali Jadelmoula <67081878+Abdelali12-codes@users.noreply.github.com>
2022-08-22 12:42:16 +0100

    Update nginx.rb

--- nginx/recipes/nginx.rb
+++ nginx/recipes/nginx.rb
@@ -3,5 +3,5 @@ service 'nginx' do
 action [:enable, :start]
 end
  file "/var/www/html/index.html" do 
-content "<h1> Hello This is a chef recipe #node[:name] and #node[:age]</h1>"
+content "<h1> Hello This is a chef recipe #{node[:name]} and #{node[:age]}</h1>"
 end
